89cbfc256e5153152df3a6c2575c706c1c8a9df5
John Fitzgerald <john.p.fitzgerald@mycit.ie>
2019-11-16 22:04:54 +0000

    extra functionality added to Hello Prog

--- HelloPython.py
+++ HelloPython.py
@@ -4,5 +4,11 @@ now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 
+firstSet = {1982,1989,1999,2002,2009,2013,2019}
+secondSet = {1985,1992,2001,2002,2005,2009,2015}
+thirdSet = {}
 
+#firstSet.difference_update(secondSet) 
+thirdSet = firstSet.union(secondSet)
+print(thirdSet)
 	
